31f864a02cb16fec4bd185fe5b76b52950416b07
kartikeyagup <kartikeyagupta@hotmail.com>
2015-06-28 02:51:31 +0530

    Finally got a working direct x window app

---
+++ DirectXLearning/DirectXLearning/Source.cpp
@@ -0,0 +1,75 @@
+#include <Windows.h>
+//#include <d3d11.h>
+
+LRESULT CALLBACK WndProc(
+	HWND hWnd,
+	UINT message,
+	WPARAM wParam,
+	LPARAM lParam)
+{
+	switch (message)
+	{
+	case WM_DESTROY: PostQuitMessage(0); break;
+	default: return DefWindowProc(hWnd, message, wParam, lParam);
+	}
+	return 0;
+}
+
+
+
+int WINAPI wWinMain(
+	HINSTANCE hInstance,
+	HINSTANCE hPrevInstance,
+	LPWSTR lpCmdLine,
+	int nCmdShow)
+{
+	WNDCLASSEX wcex;
+	wcex.cbSize = sizeof(WNDCLASSEX);
+	wcex.style = CS_HREDRAW | CS_VREDRAW;
+	wcex.lpfnWndProc = WndProc;
+	wcex.cbClsExtra = 0;
+	wcex.cbWndExtra = 0;
+	wcex.hInstance = hInstance;
+	wcex.hIcon = NULL;
+	wcex.hCursor = LoadCursor(nullptr, IDC_ARROW);
+	wcex.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
+	wcex.lpszMenuName = nullptr;
+	wcex.lpszClassName = L"MainWindow";
+	wcex.hIconSm = NULL;
+	if (!RegisterClassEx(&wcex)) return 0;
+
+	RECT rc = { 0,0,800,600 };
+	AdjustWindowRect(&rc, WS_OVERLAPPEDWINDOW, FALSE);
+	HWND hwnd = CreateWindow(
+		L"MainWindow",
+		L"Direct 3D Vid",
+		WS_OVERLAPPED | WS_CAPTION | WS_SYSMENU | WS_MINIMIZEBOX,
+		CW_USEDEFAULT,
+		CW_USEDEFAULT,
+		rc.right - rc.left,
+		rc.bottom - rc.left,
+		nullptr,
+		nullptr,
+		hInstance,
+		nullptr);
+	
+	if (!hwnd) return 0;
+
+	ShowWindow(hwnd, nCmdShow);
+	MSG msg = { 0 };
+	while (WM_QUIT != msg.message)
+	{
+		if (PeekMessage(&msg, nullptr, 0, 0, PM_REMOVE))
+		{
+			TranslateMessage(&msg);
+			DispatchMessage(&msg);
+		}
+		else
+		{
+
+		}
+	}
+
+	return 0;
+}
+
